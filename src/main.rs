5840d6d471624417aaaa3534e0ec49209122a8c2
David Heimann <d@dmeh.net>
2023-06-11 10:46:41 -0400

    Add clap and create a basic outline of our CLIs API

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{arg, Command, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("oi")
+        .about("Get AI powered short answers to questions")
+        .version("0.1.0")
+        .arg(
+            arg!(--configure "Opens the configuration file in your $EDITOR")
+                .action(ArgAction::SetTrue)
+                .required(false),
+        )
+        .arg(
+            arg!([question] "The question to ask the AI")
+                .required(false),
+        )
+        .get_matches();
+
+    if matches.get_flag("configure") {
+        println!("Opening configuration...");
+    } else if let Some(question) = matches.get_one::<String>("question") {
+        println!("Question: {}", question);
+    }
 }
+
